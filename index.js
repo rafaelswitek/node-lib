c80bb3c7bfd93bc989125cbbcdb77612c6e54585
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2023-04-08 20:45:45 -0300

    03

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import fs from 'fs'
 import chalk from 'chalk'
 
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+    const capturas = [...texto.matchAll(regex)]
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados
+}
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretorio'))
 }
@@ -9,7 +16,9 @@ async function pegaArquivo(caminhoArquivo) {
     try {
         const encoding = 'utf-8'
         const texto = await fs.promises.readFile(caminhoArquivo, encoding)
-        console.log(chalk.green(texto))
+        const links = extraiLinks(texto)
+        // const color = chalk.green(links)
+        console.log(links)
     } catch (erro) {
         trataErro(erro)
     }
