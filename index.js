de3f1a72c59fb921e14451ca8955d55bba0ad995
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2023-04-08 18:58:42 -0300

    02

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import chalk from 'chalk';
+import fs from 'fs'
+import chalk from 'chalk'
 
-console.log(chalk.blue('Hello world!'));
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretorio'))
+}
+
+async function pegaArquivo(caminhoArquivo) {
+    try {
+        const encoding = 'utf-8'
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding)
+        console.log(chalk.green(texto))
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+pegaArquivo('./arquivos/texto.md')
